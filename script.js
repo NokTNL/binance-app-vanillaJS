60ef65c263f7e4433a30b3326af67a23b75207e2
NokTNL <noklamtsz@gmail.com>
2022-01-29 17:09:57 +0000

    Add drop-down list for selecting currencies

--- script.js
+++ script.js
@@ -1,30 +1,23 @@
 async function fetchLatestTrade() {
   const response = await fetch(
-    "https://api.binance.com/api/v3/trades?symbol=BNBUSDT"
+    `https://api.binance.com/api/v3/trades?symbol=${cryptoType.value}${fiatType.value}`
   );
   const obj = await response.json();
   return obj[0];
 }
 
-// for testing
-async function fetchAvgPrice() {
-  const response = await fetch(
-    "https://api.binance.com/api/v3/avgPrice?symbol=BTCUSDT"
-  );
-  const obj = await response.json();
-  return obj;
-}
-
 // Main flow
 const timeEl = document.getElementById("time");
 const priceEl = document.getElementById("price");
+const cryptoType = document.getElementById("crypto-type");
+const fiatType = document.getElementById("fiat-type");
 
 setInterval(() => {
   fetchLatestTrade().then(({ time, price }) => {
     const timeString = time && new Date(time).toLocaleString();
-    const priceNum = price && parseFloat(price);
+    const priceNum = price && parseFloat(price).toFixed(2);
 
     timeEl.textContent = `Time stamp: ${timeString}`;
-    priceEl.textContent = `BTC/USDT: ${priceNum}`;
+    priceEl.textContent = `${cryptoType.value}/${fiatType.value}: ${priceNum}`;
   });
 }, 1000);

6b1bb4aef708b1a9d843b65380e43a602a6f3118
NokTNL <noklamtsz@gmail.com>
2022-01-29 12:57:41 +0000

    Add first contents

---
+++ script.js
@@ -0,0 +1,30 @@
+async function fetchLatestTrade() {
+  const response = await fetch(
+    "https://api.binance.com/api/v3/trades?symbol=BNBUSDT"
+  );
+  const obj = await response.json();
+  return obj[0];
+}
+
+// for testing
+async function fetchAvgPrice() {
+  const response = await fetch(
+    "https://api.binance.com/api/v3/avgPrice?symbol=BNBUSDT"
+  );
+  const obj = await response.json();
+  return obj;
+}
+
+// Main flow
+const timeEl = document.getElementById("time");
+const priceEl = document.getElementById("price");
+
+setInterval(() => {
+  fetchLatestTrade().then(({ time, price }) => {
+    const timeString = time && new Date(time).toLocaleString();
+    const priceNum = price && parseFloat(price);
+
+    timeEl.textContent = `Time stamp: ${timeString}`;
+    priceEl.textContent = `BNB/USDT: ${priceNum}`;
+  });
+}, 1000);

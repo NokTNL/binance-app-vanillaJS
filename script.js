567980b64ab5d2dd55e5ed5c9bab83665055b9b8
NokTNL <noklamtsz@gmail.com>
2022-01-30 11:24:32 +0000

    Use Websocket in place of fetch
    To avoid polling every 1 second using fetch. Dropdown list for choosing currency not functioning yet (stuck to the BTCUSDT stream)

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
-async function fetchLatestTrade() {
+/* async function fetchLatestTrade() {
   const response = await fetch(
     `https://api.binance.com/api/v3/trades?symbol=${cryptoType.value}${fiatType.value}`
   );
   const obj = await response.json();
   return obj[0];
-}
+} */
 
-// Main flow
 const timeEl = document.getElementById("time");
 const priceEl = document.getElementById("price");
 const cryptoType = document.getElementById("crypto-type");
 const fiatType = document.getElementById("fiat-type");
 
-setInterval(() => {
-  fetchLatestTrade().then(({ time, price }) => {
-    const timeString = time && new Date(time).toLocaleString();
-    const priceNum = price && parseFloat(price).toFixed(2);
+let socket = new WebSocket("wss://stream.binance.com:9443/ws/btcusdt@aggTrade");
 
-    timeEl.textContent = `Time stamp: ${timeString}`;
-    priceEl.textContent = `${cryptoType.value}/${fiatType.value}: ${priceNum}`;
-  });
-}, 1000);
+socket.onopen = function (e) {
+  console.log("[open] Connection established");
+};
+
+socket.onmessage = ({ data }) => {
+  console.log(`[message] Message received`);
+  const { p, T } = JSON.parse(data); // data received in JSON\
+  const timeString = new Date(T).toLocaleString();
+  const priceNum = parseFloat(p).toFixed(2);
+
+  timeEl.textContent = `Time stamp: ${timeString}`;
+  priceEl.textContent = `${cryptoType.value}/${fiatType.value}: ${priceNum}`;
+};
+
+socket.onclose = function (event) {
+  if (event.wasClean) {
+    console.log(
+      `[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`
+    );
+  } else {
+    console.log(
+      `[close] Connection died, code=${event.code} reason=${event.reason}`
+    );
+  }
+};
+
+socket.onerror = function (error) {
+  console.log(`[error] ${error.message}`);
+};

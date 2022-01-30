80964cd92533ea7d77b547882a49e5d6e847f24c
NokTNL <noklamtsz@gmail.com>
2022-01-30 18:38:59 +0000

    Make drop-down menu change websocket stream
    Organised codes in different functions to make managing it easier

--- script.js
+++ script.js
@@ -1,44 +1,64 @@
-/* async function fetchLatestTrade() {
-  const response = await fetch(
-    `https://api.binance.com/api/v3/trades?symbol=${cryptoType.value}${fiatType.value}`
-  );
-  const obj = await response.json();
-  return obj[0];
-} */
-
-const timeEl = document.getElementById("time");
-const priceEl = document.getElementById("price");
 const cryptoType = document.getElementById("crypto-type");
 const fiatType = document.getElementById("fiat-type");
+const timeEl = document.getElementById("time");
+const priceEl = document.getElementById("price");
 
-let socket = new WebSocket("wss://stream.binance.com:9443/ws/btcusdt@aggTrade");
+let socket;
 
-socket.onopen = function (e) {
-  console.log("[open] Connection established");
-};
+function addSocket(cryptoTypeUpper, fiatTypeUpper) {
+  const crypto = cryptoTypeUpper.toLowerCase();
+  const fiat = fiatTypeUpper.toLowerCase();
 
-socket.onmessage = ({ data }) => {
-  console.log(`[message] Message received`);
-  const { p, T } = JSON.parse(data); // data received in JSON\
+  socket = new WebSocket(
+    `wss://stream.binance.com:9443/ws/${crypto}${fiat}@aggTrade`
+  );
+
+  timeEl.textContent = `Time stamp: loading...`;
+  priceEl.textContent = `${cryptoTypeUpper}/${fiatTypeUpper}: loading...`;
+
+  socket.onopen = function (e) {
+    console.log("[open] Connection established");
+  };
+
+  socket.onmessage = ({ data }) => {
+    console.log(`[message] Message received`);
+    handleData(data);
+  };
+
+  socket.onclose = function (event) {
+    if (event.wasClean) {
+      console.log(
+        `[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`
+      );
+    } else {
+      console.log(
+        `[close] Connection died, code=${event.code} reason=${event.reason}`
+      );
+    }
+  };
+
+  socket.onerror = function (error) {
+    console.log(`[error] ${error.message}`);
+  };
+}
+
+function handleData(data) {
+  const { p, T } = JSON.parse(data); // data received in JSON
   const timeString = new Date(T).toLocaleString();
   const priceNum = parseFloat(p).toFixed(2);
 
   timeEl.textContent = `Time stamp: ${timeString}`;
   priceEl.textContent = `${cryptoType.value}/${fiatType.value}: ${priceNum}`;
-};
+}
 
-socket.onclose = function (event) {
-  if (event.wasClean) {
-    console.log(
-      `[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`
-    );
-  } else {
-    console.log(
-      `[close] Connection died, code=${event.code} reason=${event.reason}`
-    );
-  }
-};
+function handleTypeChange(event) {
+  console.log(`You have chosen: ${event.target.value}`);
+  socket.close();
+  addSocket(cryptoType.value, fiatType.value);
+}
 
-socket.onerror = function (error) {
-  console.log(`[error] ${error.message}`);
-};
+// Main flow
+addSocket(cryptoType.value, fiatType.value);
+
+fiatType.addEventListener("change", handleTypeChange);
+cryptoType.addEventListener("change", handleTypeChange);

2347c277f09b1d42a201d2804a98f040efe3f9f6
NokTNL <noklamtsz@gmail.com>
2022-01-31 20:33:32 +0000

    Refactor to modular code
    To organise code flow better and avoid declaring global variables so to let modules perform single role only

--- script.js
+++ script.js
@@ -1,78 +1,5 @@
-async function fetchLatestTrade(cryptoTypeUpper, fiatTypeUpper) {
-  try {
-    const response = await fetch(
-      `https://api.binance.com/api/v3/aggTrades?symbol=${cryptoTypeUpper}${fiatTypeUpper}`
-    );
-    const obj = await response.json();
-    const { T, p } = obj[0];
-    handleData(T, p);
-  } catch {
-    alert(
-      "Failed to get the info. Maybe the pair you chose is not available on Binance."
-    );
-  }
-}
-
-function addSocket(cryptoTypeUpper, fiatTypeUpper) {
-  const crypto = cryptoTypeUpper.toLowerCase();
-  const fiat = fiatTypeUpper.toLowerCase();
-
-  socket = new WebSocket(
-    `wss://stream.binance.com:9443/ws/${crypto}${fiat}@aggTrade`
-  );
-
-  socket.onopen = function () {
-    // fetchPrecision(cryptoTypeUpper, fiatTypeUpper);
-    fetchLatestTrade(cryptoTypeUpper, fiatTypeUpper); // To get the latest price without waiting new trades to happen
-    console.log("[open] Connection established");
-  };
-
-  socket.onmessage = ({ data }) => {
-    console.log(`[message] Message received`);
-    const { T, p } = JSON.parse(data);
-    handleData(T, p);
-  };
-
-  socket.onclose = function (event) {
-    if (event.wasClean) {
-      console.log(
-        `[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`
-      );
-    } else {
-      console.log(
-        `[close] Connection died, code=${event.code} reason=${event.reason}`
-      );
-    }
-  };
-
-  socket.onerror = function (error) {
-    console.log(`[error] ${error.message}`);
-  };
-}
-
-function handleData(time, price) {
-  const timeString = new Date(time).toLocaleString();
-  const priceNum = parseFloat(price);
-
-  timeEl.textContent = `Time stamp: ${timeString}`;
-  priceEl.textContent = `${cryptoType.value}/${fiatType.value}: ${priceNum}`;
-}
-
-function handleTypeChange(event) {
-  console.log(`You have chosen: ${event.target.value}`);
-  timeEl.textContent = `Time stamp: loading...`;
-  priceEl.textContent = `${cryptoType.value}/${fiatType.value}: loading...`;
-
-  socket.close();
-  addSocket(cryptoType.value, fiatType.value);
-}
-
-// Main flow
-const cryptoType = document.getElementById("crypto-type");
-const fiatType = document.getElementById("fiat-type");
-const timeEl = document.getElementById("time");
-const priceEl = document.getElementById("price");
-
+"use strict";
+// Database
 const cryptoList = [
   { symbol: "BTC", name: "Bitcoin" },
   { symbol: "ETH", name: "Ethereum" },
@@ -92,23 +19,118 @@ const fiatList = [
   { symbol: "GBP", name: "British Pound" },
 ];
 
-for (coin of cryptoList) {
-  const option = document.createElement("option");
-  option.value = coin.symbol;
-  option.textContent = `${coin.name} (${coin.symbol})`;
-  cryptoType.appendChild(option);
+// For fetching latest data once
+async function fetchLatestTrade(cryptoTypeUpper, fiatTypeUpper) {
+  try {
+    const response = await fetch(
+      `https://api.binance.com/api/v3/aggTrades?symbol=${cryptoTypeUpper}${fiatTypeUpper}`
+    );
+    const obj = await response.json();
+    const { T: time, p: price } = obj[0];
+    DOMMod.updateData(time, price);
+  } catch {
+    alert(
+      "Failed to get the info. Maybe the pair you chose is not available on Binance."
+    );
+  }
 }
 
-for (coin of fiatList) {
-  const option = document.createElement("option");
-  option.value = coin.symbol;
-  option.textContent = `${coin.name} (${coin.symbol})`;
-  fiatType.appendChild(option);
-}
+// WebSocket management
+const socketMod = (function () {
+  let _socket;
 
-let socket;
-fetchLatestTrade(cryptoType.value, fiatType.value);
-addSocket(cryptoType.value, fiatType.value);
+  const addSocket = (cryptoTypeUpper, fiatTypeUpper) => {
+    const crypto = cryptoTypeUpper.toLowerCase();
+    const fiat = fiatTypeUpper.toLowerCase();
 
-fiatType.addEventListener("change", handleTypeChange);
-cryptoType.addEventListener("change", handleTypeChange);
+    _socket = new WebSocket(
+      `wss://stream.binance.com:9443/ws/${crypto}${fiat}@aggTrade`
+    );
+
+    _socket.onopen = function () {
+      fetchLatestTrade(cryptoTypeUpper, fiatTypeUpper); // To get the latest price without waiting new trades to happen
+      console.log(
+        `[open] Connection established for ${cryptoTypeUpper}/${fiatTypeUpper}`
+      );
+    };
+
+    _socket.onmessage = ({ data }) => {
+      console.log(
+        `[message] Message received for ${cryptoTypeUpper}/${fiatTypeUpper}`
+      );
+      const { T: time, p: price } = JSON.parse(data);
+      DOMMod.updateData(time, price);
+    };
+
+    _socket.onclose = function (event) {
+      if (event.wasClean) {
+        console.log(
+          `[close] Connection for ${cryptoTypeUpper}/${fiatTypeUpper} closed cleanly, code=${event.code} reason=${event.reason}`
+        );
+      } else {
+        console.log(
+          `[close] Connection for ${cryptoTypeUpper}/${fiatTypeUpper} died, code=${event.code} reason=${event.reason}`
+        );
+      }
+    };
+
+    _socket.onerror = function (error) {
+      console.log(`[error] ${error.message}`);
+    };
+  };
+
+  const closeSocket = () => {
+    _socket.close();
+  };
+  return { addSocket, closeSocket };
+})();
+
+// DOM module
+const DOMMod = (function () {
+  const _cryptoType = document.getElementById("crypto-type");
+  const _fiatType = document.getElementById("fiat-type");
+  const _timeEl = document.getElementById("time");
+  const _priceEl = document.getElementById("price");
+
+  const _handleTypeChange = (event) => {
+    console.log(`You have chosen: ${_cryptoType.value}/${_fiatType.value}`);
+    _timeEl.textContent = `Time stamp: loading...`;
+    _priceEl.textContent = `${_cryptoType.value}/${_fiatType.value}: loading...`;
+
+    socketMod.closeSocket();
+    socketMod.addSocket(_cryptoType.value, _fiatType.value);
+  };
+
+  const init = () => {
+    for (const coin of cryptoList) {
+      const option = document.createElement("option");
+      option.value = coin.symbol;
+      option.textContent = `${coin.name} (${coin.symbol})`;
+      _cryptoType.appendChild(option);
+    }
+
+    for (const coin of fiatList) {
+      const option = document.createElement("option");
+      option.value = coin.symbol;
+      option.textContent = `${coin.name} (${coin.symbol})`;
+      _fiatType.appendChild(option);
+    }
+
+    _fiatType.addEventListener("change", _handleTypeChange);
+    _cryptoType.addEventListener("change", _handleTypeChange);
+  };
+
+  const updateData = (time, price) => {
+    const timeString = new Date(time).toLocaleString();
+    const priceNum = parseFloat(price);
+
+    _timeEl.textContent = `Time stamp: ${timeString}`;
+    _priceEl.textContent = `${_cryptoType.value}/${_fiatType.value}: ${priceNum}`;
+  };
+
+  return { init, updateData };
+})();
+
+// Main flow
+DOMMod.init();
+socketMod.addSocket("BTC", "BUSD");
